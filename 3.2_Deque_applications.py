4bc4dcd3b32e6c19807edbf5b2bb1a9036c303e5
kriyazhao <kriyazhao@users.noreply.github.com>
2014-04-27 11:42:31 -0400

    Create 3.2_Deque_applications.py

---
+++ 3.2_Deque_applications.py
@@ -0,0 +1,24 @@
+# ------------------------------------------------------------------------------------------------------------
+# 3.2_Deque_applications.py
+# Created on: 2013-04-11 13:55:52.00000
+# Author: Ting Zhao
+# Usage: define a function that use Deque structure. An advanced version of palinChecker() can be found in 5.1
+# Description: create some functions using Deque class:
+#              - palinChecker()     check if the input string is palindrome      O(n)
+# ------------------------------------------------------------------------------------------------------------
+
+#-----------------------------------------------------
+# check Palindrome
+def palinChecker(inputStr):
+    palinDeque = Deque()
+    for char in inputStr:
+        palinDeque.addRear(char)
+    while palinDeque.size() > 1:
+        charRear = palinDeque.removeRear()
+        charFront = palinDeque.removeFront()
+        if not charRear == charFront:
+            return False
+    return True
+
+print palinChecker("radar")
+print palinChecker("dddsseddd")
